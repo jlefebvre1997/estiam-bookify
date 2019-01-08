e461dcc8924d949132fb014717b474378ef501c8
jleleu42 <jlu59@live.fr>
2019-01-08 16:04:53 +0100

    Updated .gitignore

---
+++ src/AppBundle/Entity/Contain.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 08/01/19
+ * Time: 15:41
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Contain
+ * @ORM\Entity
+ */
+class Contain
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type = "integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Annonce", mappedBy="annonces")
+     */
+    private $annonce;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Book", mappedBy="books")
+     */
+    private $book;
+
+
+    /**
+     * @ORM\Column(type ="integer", nullable=false)
+     */
+
+    private $qte;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnnonce()
+    {
+        return $this->annonce;
+    }
+
+    /**
+     * @param mixed $annonce
+     */
+    public function setAnnonce($annonce)
+    {
+        $this->annonce = $annonce;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBook()
+    {
+        return $this->book;
+    }
+
+    /**
+     * @param mixed $book
+     */
+    public function setBook($book)
+    {
+        $this->book = $book;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQte()
+    {
+        return $this->qte;
+    }
+
+    /**
+     * @param mixed $qte
+     */
+    public function setQte($qte)
+    {
+        $this->qte = $qte;
+    }
+
+
+
+
+}
