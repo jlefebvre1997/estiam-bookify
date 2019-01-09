53f2874a279d380aa30c18fb3a6f72ec8dbb08d3
jleleu42 <jlu59@live.fr>
2019-01-09 11:49:29 +0100

    Ajout logo2

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,20 +2,20 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @Template("home/index.html.twig")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return [];
     }
 }
