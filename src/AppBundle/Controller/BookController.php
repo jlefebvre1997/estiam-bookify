220afd1a555c3a3b17ea15e1dd5ffddd23256442
MRY <yannick@redison.com>
2019-01-09 12:11:37 +0100

    Added CRUD routes & Basic templates to be rendered with

---
+++ src/AppBundle/Controller/BookController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Book;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: yannickmaelraoumbe
+ * Date: 2019-01-08
+ * Time: 15:22
+ *
+ * @Route("/books")
+ */
+class BookController extends Controller {
+
+    /**
+     * @Template
+     *
+     * @Route("/", name="book_list")
+     */
+    public  function listOfBooks() {
+        return ['bite' => 'ccc'];
+    }
+
+    /**
+     * @Template
+     *
+     * @Route("/{id}", name ="get_one_book")
+     */
+     public function getOneBook(Book $book){
+        return ['book' => $book];
+     }
+
+    /**
+     * @Template
+     *
+     * @Route("/{id}/update", name="update_one_book")
+     */
+    public function update(Book $book){
+        return ['book' => $book];
+    }
+
+    /**
+     * @Template
+     *
+     * @Route("/{id}/delete", name="delete_one_book")
+     */
+    public function delete(Book $book){
+        return ['book' => $book];
+    }
+}
